f2ae1000d3f0f04b6ac97eb2825eda6a885b62b5
Matt Light <matt@paren.io>
2018-08-01 01:31:24 -0700

    Revamp the popup layout
    Use screen captures as previews

--- popup.js
+++ popup.js
@@ -1,36 +1,30 @@
 (function () {
   const doc = document;
-  const buffer = doc.createElement('tbody');
+  const buffer = doc.createElement('div');
 
   chrome.storage.local.get(['recent'], storage => {
     console.log(storage.recent);
-    for (let request of storage.recent) {
-      const tr = doc.createElement('tr');
+    for (let clip of storage.recent) {
+      const clipDiv = doc.createElement('div');
+      clipDiv.setAttribute('class', 'clip');
+      clipDiv.innerHTML = `
+        <div class="clip-screenshot" style="background-image: url(${clip.screenshot});"></div>
+        <div class="clip-details">
+          <h2><a href=""></a></h2>
+          <blockquote></blockquote>
+        </div>
+      `;
 
-      const image = doc.createElement('img');
-      image.setAttribute('src', request.screenshot);
-      image.setAttribute('height', 300);
+      const link = clipDiv.querySelector('h2 a');
+      link.setAttribute('href', clip.url);
+      link.appendChild(doc.createTextNode(clip.title ? clip.title : link.hostname));
 
-      const sourceLink = doc.createElement('a');
-      sourceLink.setAttribute('href', request.url);
-      sourceLink.appendChild(doc.createTextNode(request.title ? request.title : request.url));
+      clipDiv.querySelector('blockquote').appendChild(doc.createTextNode(clip.clipboardData));
 
-      const sourceCell = doc.createElement('td');
-      const clipCell = doc.createElement('td');
-      const imageCell = doc.createElement('td');
-
-      sourceCell.appendChild(sourceLink);
-      clipCell.appendChild(doc.createTextNode(request.clipboardData));
-      imageCell.appendChild(image);
-
-      tr.appendChild(sourceCell);
-      tr.appendChild(clipCell);
-      tr.appendChild(imageCell);
-
-      buffer.appendChild(tr);
+      buffer.appendChild(clipDiv);
     }
 
-    doc.getElementById('clips').querySelector('tbody').innerHTML = buffer.innerHTML;
+    doc.getElementById('clips').innerHTML = buffer.innerHTML;
   });
 })();
 
