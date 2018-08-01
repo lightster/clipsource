01e61a71b63cebcf4c41369a1ed5b93211258715
Matt Light <matt@paren.io>
2018-08-01 16:12:41 -0700

    Use an IIFE for the background script
    I am not sure the IIFE is solving a problem, but it makes the code
    cleaner

--- background.js
+++ background.js
@@ -1,48 +1,50 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action !== 'copy') {
-    return;
-  }
+(function() {
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action !== 'copy') {
+      return;
+    }
 
-  const clipboardPromise = new Promise((resolve, reject) => {
-    const buffer = document.getElementById('buffer');
-    buffer.value = '';
-    buffer.addEventListener('paste', event => {
-      setTimeout(() => {
-        request.clipboardData = buffer.value;
+    const clipboardPromise = new Promise((resolve, reject) => {
+      const buffer = document.getElementById('buffer');
+      buffer.value = '';
+      buffer.addEventListener('paste', event => {
+        setTimeout(() => {
+          request.clipboardData = buffer.value;
+
+          resolve();
+        });
+      });
+      buffer.select();
+
+      if (!document.execCommand('paste')) {
+        console.log('Could not retrieve contents from clipboard.');
+      }
+    });
+
+    const screenshotPromise = new Promise((resolve, reject) => {
+      chrome.tabs.captureVisibleTab(null, {format: 'png'}, dataUri => {
+        request.screenshot = dataUri;
 
         resolve();
       });
     });
-    buffer.select();
 
-    if (!document.execCommand('paste')) {
-      console.log('Could not retrieve contents from clipboard.');
-    }
-  });
+    Promise.all([clipboardPromise, screenshotPromise]).then(() => {
+      chrome.storage.local.get(['recent', 'history'], storage => {
+        if (!storage.recent) {
+          storage.recent = [];
+        }
+        if (!storage.history) {
+          storage.history = [];
+        }
 
-  const screenshotPromise = new Promise((resolve, reject) => {
-    chrome.tabs.captureVisibleTab(null, {format: 'png'}, dataUri => {
-      request.screenshot = dataUri;
+        storage.recent.unshift(request);
+        storage.recent = storage.recent.slice(0, 10);
 
-      resolve();
+        storage.history.push(request);
+
+        chrome.storage.local.set(storage);
+      });
     });
   });
-
-  Promise.all([clipboardPromise, screenshotPromise]).then(() => {
-    chrome.storage.local.get(['recent', 'history'], storage => {
-      if (!storage.recent) {
-        storage.recent = [];
-      }
-      if (!storage.history) {
-        storage.history = [];
-      }
-
-      storage.recent.unshift(request);
-      storage.recent = storage.recent.slice(0, 10);
-
-      storage.history.push(request);
-
-      chrome.storage.local.set(storage);
-    });
-  });
-});
+})();
