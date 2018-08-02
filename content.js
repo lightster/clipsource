e16d35d0b66b4f41c0cb85f2e032f91179b27cc8
Matt Light <matt@paren.io>
2018-08-01 23:55:44 -0700

    Add Open Graph meta-data to history

--- content.js
+++ content.js
@@ -1,9 +1,15 @@
 (function () {
   document.addEventListener('copy', () => {
     setTimeout(() => {
+      let og = {};
+      for (let tag of document.querySelectorAll('meta[property^="og:"]')) {
+        og[tag.getAttribute('property').substring(3)] = tag.getAttribute('content');
+      }
+
       chrome.runtime.sendMessage({
         action: 'copy',
         title: document.title,
+        og: og,
         url: location.href
       });
     });
