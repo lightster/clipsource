873c795bfe9323b00bdf515c2f83ae84e208f01a
Matt Light <matt@paren.io>
2018-08-12 21:51:42 -0700

    Remove unused, hidden DOM for clip list

--- index.js
+++ index.js
@@ -28,17 +28,7 @@
         <div class="clip-screenshot" style="background-image: url(${clip.thumbnail.dataUrl});">
           <div class="clip-summary">${summary}</div>
         </div>
-        <div class="clip-details">
-          <h2><a href="" target="_blank"></a></h2>
-          <blockquote></blockquote>
-        </div>
       `;
-
-      const link = clipDiv.querySelector('h2 a');
-      link.setAttribute('href', clip.url);
-      link.appendChild(doc.createTextNode(clip.title ? clip.title : link.hostname));
-
-      clipDiv.querySelector('blockquote').appendChild(doc.createTextNode(clip.clipboard.plain));
 
       buffer.appendChild(clipDiv);
       doc.getElementById('clips').innerHTML = buffer.innerHTML;
