eca59c7cdde361e9d6a9201d6edaf030e46de667
Matt Light <matt@paren.io>
2018-09-03 23:37:05 -0700

    Remove unnecessary IIFE in background script
    The background script is loaded as an ES6 module, so an IIFE should be
    unnecessary

--- background.js
+++ background.js
@@ -2,28 +2,26 @@ import Clip from './clip.js';
 import Clipboard from './clipboard.js';
 import Image from './image.js';
 
-(function () {
-  const saveClipboard = async clip => {
-    clip.clipboard = {
-      plain: await Clipboard.capturePlainText(clip),
-      html: await Clipboard.captureHtml(clip)
-    };
+const saveClipboard = async clip => {
+  clip.clipboard = {
+    plain: await Clipboard.capturePlainText(clip),
+    html: await Clipboard.captureHtml(clip)
   };
+};
 
-  const saveImages = async clip => {
-    const {screenshot, image} = await Image.captureScreenshot();
-    clip.screenshot = screenshot;
-    clip.thumbnail = await Image.createThumbnail(clip, image);
-  };
+const saveImages = async clip => {
+  const {screenshot, image} = await Image.captureScreenshot();
+  clip.screenshot = screenshot;
+  clip.thumbnail = await Image.createThumbnail(clip, image);
+};
 
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    const clip = Clip.Init(request);
-    Promise.all([saveClipboard(clip), saveImages(clip)]).then(() => {
-      if (request.action !== 'copy') {
-        return;
-      }
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const clip = Clip.Init(request);
+  Promise.all([saveClipboard(clip), saveImages(clip)]).then(() => {
+    if (request.action !== 'copy') {
+      return;
+    }
 
-      clip.save();
-    });
+    clip.save();
   });
-})();
+});
